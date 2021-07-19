60f46d85141793b8d10d8e501542a6fc22940d52
arunansh-moglix <arunansh.singh@moglix.com>
2021-07-19 22:38:46 +0530

    first commit

---
+++ employeeManager/src/main/java/com/training/employeeManager/EmployeeManagerApplication.java
@@ -0,0 +1,15 @@
+package com.training.employeeManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class EmployeeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagerApplication.class, args);
+	}
+
+}
---
+++ employeeManager/src/main/java/com/training/employeeManager/repo/EmployeeRepo.java
@@ -0,0 +1,15 @@
+ package com.training.employeeManager.repo;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.training.employeeManager.model.Employee;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+
+	void deleteEmployeeById(Long id);
+
+	Optional<Employee> findEmployeeById(Long id);
+
+}
